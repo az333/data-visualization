387f8e8f11746446cafaf300e36839ac6cca909a
Xavier Chen <xchen5@stuy.edu>
2018-04-18 20:24:14 -0400

    Request stuff added

--- app.py
+++ app.py
@@ -10,10 +10,16 @@ def root():
 
 
 #---------------------ONE STATE-------------------------------
-@app.route("/<id>")
+@app.route("/<id>", methods = ['GET', 'POST'])
 def state_graphs(id):
-    if id == "NY.html":
+    if id == "NY":
         return "hi"
+
+@app.route("/compare/", methods = ['GET', 'POST'])
+def compare():
+    state1 = request.args['state1']
+    state2 = request.args['state2']
+    return state1 + " " + state2
 
 if __name__ == "__main__":
     app.run(debug=True)
