9d3b1999e21dcdadec56b8b1b3e53e8678c9c92c
MdAbedin <mabedin1@stuy.edu>
2018-04-19 12:00:14 -0400

    added data, got state pages working

--- app.py
+++ app.py
@@ -1,8 +1,49 @@
 from flask import Flask, render_template, redirect, url_for,request, session, flash
-
+import json
 
 app = Flask(__name__)
 
+#-----------------------DATA PARSING--------------------------
+def parseData():
+    inFile = open('./data/data.csv', 'rU')
+    dataStr = inFile.read()
+    statesStr = dataStr.split('\n\n')
+    data = dict()
+    
+    for state in statesStr:
+        stateData = dict()
+        stateData['causes'] = []
+
+        lines = state.split('\n')
+        causes = lines[1:11]
+        
+        for cause in causes:
+            entries = cause.split(';')
+            
+            #name
+            entries[0] = entries[0][entries[0].find(' ')+1:]
+            
+            #deaths
+            entries[1] = int(entries[1].replace(',', ''))
+            
+            #rate
+            entries[2] = float(entries[2])
+            
+            #rank
+            entries[3] = int(entries[3].replace(' (tie)', '')[:-2])
+            
+            #us rate
+            entries[4] = float(entries[4])
+            
+            stateData["causes"].append(entries)
+            
+        abbrev = state[0:2]
+        data[str(abbrev)] = stateData
+
+    return data
+
+data = parseData()
+print data.keys()
 #-----------------------MAIN MAP PAGE--------------------------
 @app.route("/", methods = ['GET', 'POST'])
 def root():
@@ -10,10 +51,9 @@ def root():
 
 
 #---------------------ONE STATE-------------------------------
-@app.route("/<id>", methods = ['GET', 'POST'])
-def state_graphs(id):
-    if id == "NY":
-        return "hi"
+@app.route("/<stateId>", methods = ['GET', 'POST'])
+def state_graphs(stateId):
+    return render_template('state.html', state=stateId, causes=data[str(stateId)]['causes'])
 
 @app.route("/compare/", methods = ['GET', 'POST'])
 def compare():
--- utils/parse.py
+++ utils/parse.py
@@ -1,19 +1,37 @@
-import csv
-
-input_file = csv.DictReader(open("FIREARMS2016.csv"))
-
+inFile = open('../data/data.csv', 'rU')
+dataStr = inFile.read()
+statesStr = dataStr.split('\n\n')
+print statesStr
 data = dict()
 
-for row in input_file:
-    del row["URL"]
-    state = row["STATE"]
-    del row["STATE"]
-    data[state] = row
+for state in statesStr:
+    stateData = dict()
+    stateData['top'] = []
 
-thing = ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
-	 "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
-	 "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
-	 "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
-	 "WI", "MO", "AR", "OK", "KS", "LS", "VA"]
+    lines = state.split('\n')
+    causes = lines[1:11]
+
+    for cause in causes:
+        entries = cause.split(';')
+
+        #name
+        entries[0] = entries[0][entries[0].find(' ')+1:]
+
+        #deaths
+        entries[1] = int(entries[1].replace(',', ''))
+
+        #rate
+        entries[2] = float(entries[2])
+
+        #rank
+        entries[3] = int(entries[3].replace(' (tie)', '')[:-2])
+
+        #us rate
+        entries[4] = float(entries[4])
+        
+        stateData["top"].append(entries)
+    
+    abbrev = state[0:2]
+    data[abbrev] = stateData
 
 print data
