6f3ac3e252898af8762f7cd9701a484df1ff59a5
Xavier Chen <xchen5@stuy.edu>
2018-04-21 22:49:50 -0400

    Finished comparison!

--- app.py
+++ app.py
@@ -9,40 +9,42 @@ def parseData():
     dataStr = inFile.read()
     statesStr = dataStr.split('\n\n')
     data = dict()
-    
+
     for state in statesStr:
         stateData = dict()
         stateData['causes'] = []
 
         lines = state.split('\n')
         causes = lines[1:11]
-        
+
         for cause in causes:
             entries = cause.split(';')
-            
+
             #name
             entries[0] = entries[0][entries[0].find(' ')+1:]
-            
+
             #deaths
             entries[1] = int(entries[1].replace(',', ''))
-            
+
             #rate
             entries[2] = float(entries[2])
-            
+
             #rank
             entries[3] = int(entries[3].replace(' (tie)', '')[:-2])
-            
+
             #us rate
             entries[4] = float(entries[4])
-            
+
             stateData["causes"].append(entries)
-            
+
         abbrev = state[0:2]
         data[str(abbrev)] = stateData
 
     return data
 
 data = parseData()
+causes=data["NY"]['causes'][0][0]
+print causes
 print data.keys()
 #-----------------------MAIN MAP PAGE--------------------------
 @app.route("/", methods = ['GET', 'POST'])
@@ -59,7 +61,7 @@ def state_graphs(stateId):
 def compare():
     state1 = request.args['state1']
     state2 = request.args['state2']
-    return state1 + " " + state2
+    return render_template('compare.html', state1=state1,state2=state2, causes1 = data[str(state1)]['causes'], causes2 = data[str(state2)]['causes'])
 
 if __name__ == "__main__":
     app.run(debug=True)
